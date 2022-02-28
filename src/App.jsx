a2f63245246560963192957763a67cdc84f074a8
kunalr-99 <rkunal3099@gmail.com>
2022-02-28 11:30:30 +0530

    Commit hi to hai

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 // import UseState1 from "./Components/UseState1";
-// import FormValidation from "./Components/FormValidation";
-import ApiHandling from "./Components/ApiHandling";
+import FormValidation from "./Components/FormValidation";
+// import ApiHandling from "./Components/ApiHandling";
 
 const App = () => {
   return (
     <>
       {/* <UseState1 /> */}
-      {/* <FormValidation /> */}
-      <ApiHandling />
+      <FormValidation />
+      {/* <ApiHandling /> */}
     </>
   );
 };
